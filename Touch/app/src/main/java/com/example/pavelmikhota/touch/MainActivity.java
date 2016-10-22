406b6dbe1f09f66921c949b0fb8c61e6d22bd49f
Pavel Mikhota <pavelmikhota@live.ru>
2016-10-22 22:53:47 +0300

    add lab8

---
+++ Touch/app/src/main/java/com/example/pavelmikhota/touch/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.pavelmikhota.touch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    TextView result;
+
+    double x,y;
+    String down, move, up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.textView);
+        result.setOnTouchListener(this);
+        result = new TextView(this);
+        result.setOnTouchListener(this);
+        setContentView(result);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+
+        x = event.getX();
+        y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                down = "Down: X:" + x + ", Y:" + y;
+                move = "";
+                up = "";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                move = "Move: X:" + x + ", Y:" + y;
+                break;
+            case MotionEvent.ACTION_UP:
+                move = "";
+                up = "Up: X" + x + ", Y:" + y;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        result.setText(down + "\n" + up + "\n" + move);
+        // здесь можем вывести нужные надписи на papa
+        return true;
+    }
+
+}
